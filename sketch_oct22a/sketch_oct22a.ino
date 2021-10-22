57950ef5e1e4235d20c6dc6fcdf915a0e7ae18d7
jcy1511 <jcy1511@gmail.com>
2021-10-22 16:49:04 +0900

    dsfasdf

--- sketch_oct22a/sketch_oct22a.ino
+++ sketch_oct22a/sketch_oct22a.ino
@@ -1,19 +1,20 @@
-#include <DHT.h>
-#define DHTPIN 2               // 핀설정
-#define DHTTYPE DHT22      // DHT type
-DHT dht(DHTPIN, DHTTYPE);
+#include <Servo.h>
 
+const int SERVO = 10;
+Servo servo;
 void setup() {
-  Serial.begin(9600);            // 통신속도 설정
+  servo.attach(SERVO);
 }
 
 void loop() {
-  int h = dht.readHumidity();
-  int t = dht.readTemperature();
-
-  Serial.print("humidity:");          // ‘시리얼 플로터’ 사용위해 이부분 주석 필요
-  Serial.println(h);                  // 습도값 출력
-  Serial.print("temperature:");       // ‘시리얼 플로터’ 사용위해 이부분 주석 필요
-  Serial.println(t);                  // 온도값 출력
-  delay(1000);
+int angle=0;
+for(angle=0;angle<=180;angle++){
+servo.write(angle);
+delay(10);
+}
+for(angle=180;angle>=0;angle--) {
+  servo.write(angle);
+  delay(20);
+}
+servo.detach();
 }
