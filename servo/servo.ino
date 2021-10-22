fe6e2cb46671c5ef03ee6d90b94a6826eaa87515
jcy1511 <jcy1511@gmail.com>
2021-10-22 19:59:59 +0900

    Create servo.ino

---
+++ servo/servo.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+const int SERVO = 10;
+Servo servo;
+void setup() {
+  servo.attach(SERVO);
+}
+
+void loop() {
+  int angle=0;
+  for(angle=0;angle<=180;angle++){
+    servo.write(angle);
+    delay(20);
+  }
+  for(angle=180;angle>=0;angle--) {
+    servo.write(angle);
+    delay(20);
+  }
+  servo.detach();
+}
